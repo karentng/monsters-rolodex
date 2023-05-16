3f9e2e2b1e29a570578cb7f538920afc6adc4052
Karent Narvaez <karentnathalia@gmail.com>
2023-05-16 12:02:28 -0400

    React App ClassComponents

---
+++ src/components/card-list/CardList.jsx
@@ -0,0 +1,18 @@
+import { Component } from "react";
+import './card-list.css';
+import Card from "../card/Card";
+
+class CardList extends Component {
+  render() {
+    const { monsters } = this.props;
+    return (
+      <div className='card-list'>
+        {monsters.map((monster) => {
+          return <Card monster={monster} />;
+        })}
+      </div>
+    );
+  }
+}
+
+export default CardList;
