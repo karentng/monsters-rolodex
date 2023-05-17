c6aa686951ca714bff74d90245fba68a67cda10b
Karent Narvaez <karentnathalia@gmail.com>
2023-05-16 23:44:47 -0400

    Add typescript

--- src/components/card-list/CardList.tsx
+++ src/components/card-list/CardList.tsx
@@ -1,7 +1,13 @@
 import './card-list.css';
 import Card from "../card/Card";
 
-const CardList = ({ monsters }) => (
+import { Monster } from '../../App';
+
+type CardListProps = {
+  monsters: Monster[];
+}
+
+const CardList = ({ monsters }: CardListProps) => (
   <div className='card-list'>
     {monsters.map((monster) => {
       return <Card monster={monster} key={monster.id}/>;
---
+++ src/components/search-box/SearchBox.tsx
@@ -0,0 +1,19 @@
+import { ChangeEvent} from "react";
+import "./search-box.css"
+
+type SearchBoxProps = {
+  className: string;
+  placeholder: string;
+  onSearchChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const SearchBox = ({ onSearchChange, placeholder, className }: SearchBoxProps) => (
+  <input
+    className={`search-box ${className}`}
+    type="search"
+    placeholder={placeholder}
+    onChange={(e) => onSearchChange}
+  />
+);
+
+export default SearchBox;
